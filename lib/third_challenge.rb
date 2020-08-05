9abdf45f2caf623d850a428a84c2ad6893c5925a
Sean B <butts.sean01@gmail.com>
2020-08-05 00:03:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,21 +13,8 @@ def third_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
-   }
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-
+     }
+  }, 
+  
   
 end
